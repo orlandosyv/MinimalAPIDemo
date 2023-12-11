71df7b4aee11e21e8793b1de8f89ee5ab8d28aa7
OrlandoYV <sergioyv@gmail.com>
2023-12-11 16:42:29 -0500

    oy  - db update

--- MagicVilla_CouponAPI/Data/CouponStore.cs
+++ MagicVilla_CouponAPI/Data/CouponStore.cs
@@ -8,7 +8,8 @@ namespace MagicVilla_CouponAPI.Data
         {
             new Coupon{Id=1, Name="10oFF", Percent=10, IsActive=true },
             new Coupon{Id=2, Name="20oFF", Percent=20, IsActive= false},
-            new Coupon{Id=3, Name="30oFF", Percent=30, IsActive= true}
+            new Coupon{Id=3, Name="30oFF", Percent=30, IsActive= true},
+            new Coupon{Id=4, Name="40off", Percent=40, IsActive= true}
         };
     }
 }
